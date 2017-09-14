3423cb390e2149c0550bf99e52f21462e027b43c
selesdepselesnul <selesdepselesnul@gmail.com>
2017-09-14 13:35:53 +0700

    add proxy

--- src/GradeTable.js
+++ src/GradeTable.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class GradeTable extends Component {
+  
+  constructor(props) {
+      super(props);
+
+      var config = {
+        headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      };
+
+      var params = new FormData();
+      params.append('nim', '41155050140062');
+
+      axios.post('https://cors-anywhere.herokuapp.com/http://www.unla.ac.id/index.php/e_akademic/c_kartuhasilstudi/grid', 
+                 params, 
+                 config)
+            .then(function (response) {
+                console.log(response);
+            })
+  }
+  
   render() {
     return (
         <table className="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Kode Matkul</th>
-                    <th>Nama Matkul</th>
-                    <th>SKS</th>
-                    <th>Nilai Huruf</th>
-                    <th>Bobot</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
         </table>
     );
   }
