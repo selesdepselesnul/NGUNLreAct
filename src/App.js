28eb281286a7dfb73d3140551469f251060f592c
selesdepselesnul <selesdepselesnul@gmail.com>
2017-09-14 13:45:42 +0700

    add react progress 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GradeTable from './GradeTable';
+import "react-progress-2/main.css"
+import Progress from "react-progress-2";
 
 class App extends Component {
   render() {
     return (
       <div className="row">
+        <Progress.Component/>
         <GradeTable/>
       </div>
     );
--- src/GradeTable.js
+++ src/GradeTable.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Progress from "react-progress-2";
 
 class GradeTable extends Component {
   
@@ -12,15 +13,7 @@ class GradeTable extends Component {
         }
       };
 
-      var params = new FormData();
-      params.append('nim', '41155050140062');
-
-      axios.post('https://cors-anywhere.herokuapp.com/http://www.unla.ac.id/index.php/e_akademic/c_kartuhasilstudi/grid', 
-                 params, 
-                 config)
-            .then(function (response) {
-                console.log(response);
-            })
+      
   }
   
   render() {
@@ -28,6 +21,19 @@ class GradeTable extends Component {
         <table className="table table-bordered">
         </table>
     );
+  }
+
+  componentDidMount() {
+    Progress.show();
+    var params = new FormData();
+    params.append('nim', '41155050140062');
+
+    axios.post('https://cors-anywhere.herokuapp.com/http://www.unla.ac.id/index.php/e_akademic/c_kartuhasilstudi/grid', 
+               params)
+          .then(function (response) {
+                console.log(response);
+                Progress.hide();
+          })
   }
 }
 
