895d2c32cf62ef2cb0dc10e3b4987ff8eacfdfc6
selesdepselesnul <selesdepselesnul@gmail.com>
2017-09-28 06:15:38 +0700

    make sure to ignore enter in search by subject name

--- src/components/SubjectFinder.js
+++ src/components/SubjectFinder.js
@@ -4,7 +4,11 @@ import Progress from "react-progress-2";
 const SUBJECT_BY_NAME_PLACEHOLDER = 'Ketik nama matkul disini untuk mencari...';
 const SubjectFinder = ({setGradeRowsBySubjectName, isShown}) => {
 
-  const onKeyUp = (event) => setGradeRowsBySubjectName(event.target.value);
+  const onKeyUp = (event) => {
+    if(event.key !== 'Enter') {
+      setGradeRowsBySubjectName(event.target.value);
+    }
+  }
 
   return (
       <div className={isShown ? "col-md-12" : "hidden"}>
